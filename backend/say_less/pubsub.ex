a3b2c56a1d8b518a3bbdd27d40a22f9bdc906769
gongahkia <angryapplegravy@gmail.com>
2025-08-07 11:38:19 +0800

    feat(app): edited

--- backend/say_less/pubsub.ex
+++ backend/say_less/pubsub.ex
@@ -2,5 +2,9 @@ defmodule SayLess.PubSub do
   @moduledoc """
   The PubSub system for the SayLess application.
   """
-  use Phoenix.PubSub
+  # We provide the required otp_app and specify the Local adapter,
+  # which is perfect for applications without a database.
+  use Phoenix.PubSub,
+    otp_app: :say_less,
+    adapter: Phoenix.PubSub.Local
 end
